1d0776fa7d8fb5db13d1eb94304c49d770fe423d
Dinesh <dineshchauhan416pd@gmail.com>
2023-03-03 23:54:24 +0530

    Contract is ready to deploy

--- Web3/contracts/NFTMarketplace.sol
+++ Web3/contracts/NFTMarketplace.sol
@@ -2,9 +2,9 @@
 pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
 
 contract NFTMarketplace is ERC721URIStorage {
@@ -122,7 +122,7 @@ contract NFTMarketplace is ERC721URIStorage {
                if(listedTokens[i+1].owner == msg.sender || listedTokens[i+1].seller == msg.sender){
                     uint id = i + 1;
 
-                    Token[] storage currentToken = listedTokens[id];
+                    Token storage currentToken = listedTokens[id];
                     myNFTs[index] = currentToken;
 
                     index += 1;
@@ -134,7 +134,7 @@ contract NFTMarketplace is ERC721URIStorage {
      function executeSale(uint256 tokenId) public payable {
 
           uint price = listedTokens[tokenId].price;
-          uint seller  = listedTokens[tokenId].seller;
+          address seller  = listedTokens[tokenId].seller;
 
           require(msg.value == price ,"Invalid amount, cannot by NFT");
 
